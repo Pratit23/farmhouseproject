3f34bde955dfbff5f41058c3951656337e4b62b6
aajinkya1203 <aajinkya1203@gmail.com>
2022-07-26 16:44:18 +0530

    Added navbar, exported routes seperately

--- src/components/global/Navbar.js
+++ src/components/global/Navbar.js
@@ -2,37 +2,49 @@ import React from 'react'
 import { CONSTANTS } from '../../config/constants'
 import { IMAGELIST } from '../../config/imageList'
 
+const NAVROUTES = [
+    {
+        path: "/gallery",
+        name: "Gallery"
+    },
+    {
+        path: "/locations",
+        name: "Locations"
+    },
+    {
+        path: "/contact-us",
+        name: "Contact Us"
+    },
+
+]
+
 function Navbar() {
     return (
-        <nav class="border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
-            <div class="container flex flex-wrap justify-between items-center mx-auto">
-                <a href="/" class="flex items-center">
-                    <img src={IMAGELIST.LOGO} class="mr-3 h-6 sm:h-9" alt={`${CONSTANTS.appName}`} />
-                    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+        <nav className="border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+            <div className="container flex flex-wrap justify-between items-center mx-auto">
+                <a href="/" className="flex items-center">
+                    <img src={IMAGELIST.LOGO} className="mr-3 h-6 sm:h-9" alt={`${CONSTANTS.appName}`} />
+                    <span className="self-center text-xl font-semibold whitespace-nowrap">
                         {CONSTANTS.appName}
                     </span>
                 </a>
-                <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" ariaControls="navbar-default" ariaExpanded="false">
+                    <span className="sr-only">Open main menu</span>
+                    <svg className="w-6 h-6" ariaHidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                 </button>
-                <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-                        <li>
-                            <a href="#" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
-                        </li>
-                        <li>
-                            <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-                        </li>
-                        <li>
-                            <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-                        </li>
-                        <li>
-                            <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Pricing</a>
-                        </li>
-                        <li>
-                            <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-                        </li>
+                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                    <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                        {
+                            NAVROUTES.map(route => {
+                                return (
+                                    <li>
+                                        <a href="#" className="block py-2 pr-4 pl-3 bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0" ariaCurrent="page">
+                                            {route.name}
+                                        </a>
+                                    </li>
+                                )
+                            })
+                        }
                     </ul>
                 </div>
             </div>
--- src/components/homepage/discoverComponent.js
+++ src/components/homepage/discoverComponent.js
@@ -5,7 +5,7 @@ function Homepage() {
     return (
         <div class="grid grid-cols-2">
             <div className="border-t-2 border-r-2 border-divider_brown" style={{ height: '36rem' }}>
-                <div class="grid grid-rows-2 grid-flow-col gap-4 p-16">
+                <div class="grid grid-rows-2" style={{ height: '100%' }}>
                     <div style={{ backgroundColor: 'red', height: '70%' }}>
                         {/* <img className="object-contain" src={IMAGELIST.DISCOVER_1} alt="discover_1" /> */}
                     </div>
