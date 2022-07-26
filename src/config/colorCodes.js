2668dae3157a116f845847e8becc41fe9f531b9f
aajinkya1203 <aajinkya1203@gmail.com>
2022-07-26 15:18:32 +0530

    Color codes pushed

--- src/config/colorCodes.js
+++ src/config/colorCodes.js
@@ -1,3 +1,9 @@
 export const ColorCodes = {
-
+    GREEN: "#487254",
+    CREAM_WHITE: "#f6f3e9",
+    LIGHT_BROWN: "#dfdcd3",
+    CHOC_BROWN: "#22150c",
+    BLACK: "#000",
+    WHITE: "#FFF",
+    DIVIDER_BROWN: "#d6d0bc"
 }
