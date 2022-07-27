8426c80ffbfa814cc0f3204e20b9ee7ef01a0642
Pratit23 <pratitbandiwadekar@gmail.com>
2022-07-28 01:12:04 +0530

    added deployment code

--- src/components/global/Navbar.js
+++ src/components/global/Navbar.js
@@ -24,7 +24,7 @@ function Navbar() {
         <nav className="border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 md:border-b-2 border-divider_brown py-7">
             <div className="container flex flex-wrap justify-between items-center mx-auto">
                 <a href="/" className="flex items-center">
-                    <img src={IMAGELIST.LOGO} className="mr-3 h-6 sm:h-9" alt={`${CONSTANTS.appName}`} />
+                    <img src={process.env.PUBLIC_URL + "/logo.png"} className="mr-3 h-6 sm:h-9" alt={`${CONSTANTS.appName}`} />
                     <span className="self-center text-xl font-semibold whitespace-nowrap">
                         {CONSTANTS.appName}
                     </span>
