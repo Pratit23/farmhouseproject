4e36c8255091948de924d269c8304489ea97db07
Pratit23 <pratitbandiwadekar@gmail.com>
2022-07-28 01:00:54 +0530

    deployment code changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { ROUTES } from "./config/routes";
 function App() {
   return (
     <div className="max-w-screen-2xl mx-auto">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           {
             ROUTES.map(route => {
